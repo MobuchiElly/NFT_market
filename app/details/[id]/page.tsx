a2044a4f4872ae9d1a5b78203d6084ff12336137
MobuchiElly <ugwueleazer@gmail.com>
2025-01-30 17:04:53 +0100

    split details page card details section into its own component

--- app/details/[id]/page.tsx
+++ app/details/[id]/page.tsx
@@ -3,8 +3,8 @@
 import { useParams } from "next/navigation"
 import cards from "@/constants/cardData"
 import { CardProps } from "@/types/components"
-import Image from "next/image";
 import Suggession from "@/components/sectionComponents/Suggession";
+import CardDetailsSection from "@/components/sectionComponents/CardDetailsSection";
 
 
 const Page = () => {
@@ -19,52 +19,7 @@ const Page = () => {
       (
       <div className="flex flex-col items-center space-y-16">
         
-        <div className="w-[78vw] h-[56rem border border-[#262840] rounded-[20px] flex gap-6 p-4">
-          <div className="flex-1">
-            <Image
-            src={card.src[0].lg}
-            className="w-full rounded-[20px]"
-            alt="image 1"
-            width={400}
-            height={520}
-            priority
-            />
-          </div>
-          <div className="flex-1">
-            <h3 className="text-white mb-2">{card.title}</h3>
-            <p className="text-body-medium-16 text-[#7780A1]">Ut amet vulputate faucibus vitae semper eget auctor. Diam tempor pulvinar ultricies dolor feugiat aliquam commodo.</p>
-            <div className="flex gap-8 my-8">
-              <div className="flex gap-4">
-                <Image
-                src={card.creator[0].src}
-                className="rounded-[20px]"
-                alt="image 1"
-                width={40}
-                height={40}
-                priority
-                />
-                <div>
-                  <p>Creator</p>
-                  <p className="h8 text-white">{card.creator[0].name}</p>
-                </div>
-              </div>
-              <div className="flex gap-4">
-                <Image
-                src={card.collection[0].src}
-                className="rounded-[20px]"
-                alt="image 1"
-                width={40}
-                height={40}
-                priority
-                />
-                <div>
-                  <p>Creator</p>
-                  <p className="h8 text-white">{card.collection[0].name}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CardDetailsSection card={card}/>
 
         <Suggession suggestionCards={remainingCards}/>
       </div>
